927ed5ab9c6d4368176c5d82a5b403830dbff121
DevXen <DevXen@Gmail.com>
2023-02-21 19:48:38 -0700

    update to version 1.2
    log file saves info, proccessing is colorized on the screen, code is optimized. added commands to: -r to remove all existing srt files, and -df to accept all the default options.

--- MusicVideoTitleSubtitles.py
+++ MusicVideoTitleSubtitles.py
@@ -1,16 +1,18 @@
-# Version 1.0
+# Version 1.2
 
 import os
 import subprocess
 import argparse
+import logging
+
+# initialize logging
+logging.basicConfig(filename='MusicVideoTitleSubtitles.log',level=logging.INFO,format='%(asctime)s.%(msecs)03d - %(message)s',datefmt='%m-%d-%Y %H:%M:%S',)
+width = os.get_terminal_size().columns
 
 # function to convert time in seconds to subtitle timestamp format
 def to_timestamp(seconds):
-    hours = int(seconds / 3600)
-    minutes = int((seconds % 3600) / 60)
-    seconds = seconds % 60
-    milliseconds = int((seconds - int(seconds)) * 1000)
-    return f"{hours:02d}:{minutes:02d}:{int(seconds):02d},{milliseconds:03d}"
+    """Converts seconds to SRT timestamp format."""
+    return f"{str(int(seconds//60)).zfill(2)}:{str(int(seconds%60)).zfill(2)},{str(int((seconds%1)*1000)).zfill(3)}"
 
 # parse command line arguments
 parser = argparse.ArgumentParser(description='Create subtitles for video files in a directory.')
@@ -18,46 +20,40 @@ parser.add_argument('-d', '--directory', help='directory path (default is curren
 parser.add_argument('-s', '--subdirs', action='store_true', help='scan subdirectories')
 parser.add_argument('-t', '--time', help='subtitle display time in seconds, or \'full\' (default is 10)')
 parser.add_argument('-p', '--position', choices=['start', 'end', 'both'], help='subtitle position (default is both)')
+parser.add_argument('-r', '--remove', action='store_true', help='remove existing subtitle files')
+parser.add_argument('-df', '--default', action='store_true', help='skip user prompts and use defaults')
 
 args = parser.parse_args()
 
-# get directory path from user input or command line argument, default to current directory
-directory = args.directory or input("Enter directory path (default is current directory): ") or "."
-
-# get option to scan subdirectories from command line argument or user input
-if args.subdirs:
-    scan_subdirs = True
-else:
-    scan_subdirs = input("Scan subdirectories? (Y/n, default is Y): ").lower() in ["y", ""]
-
-# get subtitle display time option from command line argument or user input
-if args.time:
-    subtitle_display_time = args.time
-else:
-    subtitle_display_time = input("Enter subtitle display time in seconds, or 'full' (default is 10): ").lower()
-
-if subtitle_display_time in ['f', 'full']:
-    subtitle_display_time = 'full'
-else:
+# set defaults
+directory = '.' if args.default else args.directory or input("Enter directory path (default is current directory): ") or '.'
+scan_subdirs = True if args.subdirs or args.default else input("Scan subdirectories? (Y/n, default is Y): ").lower() in ['y', '']
+subtitle_display_time = 'full' if args.default else args.time or input("Enter subtitle display time in seconds, or 'full' (default is 10): ").lower()
+if subtitle_display_time not in ['f', 'full']:
     try:
         subtitle_display_time = int(subtitle_display_time)
     except ValueError:
         subtitle_display_time = 10
+subtitle_position = 'both' if args.default else args.position or input("Enter subtitle position, 'start', 'end', or 'both' (default is 'both'): ").lower()
+subtitle_position = 'start' if subtitle_position in ['s', 'start'] else 'end' if subtitle_position in ['e', 'end'] else 'both'
 
-# get subtitle position option from command line argument or user input
-if args.position:
-    subtitle_position = args.position
-else:
-    subtitle_position = input("Enter subtitle position, 'start', 'end', or 'both' (default is 'both'): ").lower()
+# remove existing subtitle files if requested
+if args.remove:
+    for dirpath, dirnames, filenames in os.walk(directory):
+        for filename in filenames:
+            filepath = os.path.join(dirpath, filename)
+            srt_filename = os.path.splitext(filepath)[0] + ".srt"
 
-if subtitle_position in ['s', 'start']:
-    subtitle_position = 'start'
-elif subtitle_position in ['e', 'end']:
-    subtitle_position = 'end'
-else:
-    subtitle_position = 'both'
-
+            # check if file is a subtitle file
+            ext = os.path.splitext(filename)[1].lower()
+            if ext == ".srt":
+                # remove existing subtitle file
+                os.remove(os.path.join(dirpath, filename))
+                print(f"\033[91mDELETED: {filename}\033[0;0m")
+                print("\033[1;90m " + '-' * (width-2) + "\033[0;0m")
+                logging.warning(f"DELETED: {srt_filename}" + "\n" + " " + "-" * 198)
 # loop through files in directory (and subdirectories if selected)
+
 for dirpath, dirnames, filenames in os.walk(directory):
     for filename in filenames:
         # check if file is a video file
@@ -68,7 +64,11 @@ for dirpath, dirnames, filenames in os.walk(directory):
 
             # check if subtitle file already exists, and skip creating it if it does
             if os.path.exists(os.path.join(dirpath, subtitle_filename)):
-                print(f"Skipping subtitle file creation for {filename}, subtitle file already exists.")
+                filepath = os.path.join(dirpath, filename)
+                srt_filename = os.path.splitext(filepath)[0] + ".srt"
+                print(f"\033[95mSKIPPING: {filename} (Already Exists.)\033[0;0m")
+                print("\033[1;90m " + '-' * (width-2) + "\033[0;0m")
+                logging.warning(f"SKIPPING: {srt_filename} (Already Exists)" + "\n" + " " + "-" * 198)
                 continue
 
             # get video duration using ffprobe
@@ -78,33 +78,26 @@ for dirpath, dirnames, filenames in os.walk(directory):
             duration = float(result.stdout.strip())
 
             # create subtitle file
-            with open(os.path.join(dirpath, subtitle_filename), "w") as f:
-                if subtitle_position == 'start':
-                    # write subtitle for first 10 seconds
-                    f.write("1\n")
+            with open(os.path.join(dirpath, subtitle_filename), "w", encoding='utf-8') as f:
+                f.write("1\n")
+                if subtitle_display_time == 'full':
+                    f.write(f"{to_timestamp(0)} --> {to_timestamp(duration)}\n")
+                elif subtitle_position == 'start':
                     f.write(f"{to_timestamp(0)} --> {to_timestamp(subtitle_display_time)}\n")
-                    f.write(f"{os.path.splitext(filename)[0]}\n")
-                    f.write("\n")
                 elif subtitle_position == 'end':
-                    # write subtitle for last 10 seconds
-                    f.write("1\n")
                     f.write(f"{to_timestamp(duration-subtitle_display_time)} --> {to_timestamp(duration)}\n")
-                    f.write(f"{os.path.splitext(filename)[0]}\n")
-                    f.write("\n")
                 else:
-                    # write subtitle for first 10 seconds
-                    f.write("1\n")
                     f.write(f"{to_timestamp(0)} --> {to_timestamp(subtitle_display_time)}\n")
-                    f.write(f"{os.path.splitext(filename)[0]}\n")
-                    f.write("\n")
-
-                    # write subtitle for last 10 seconds
                     f.write("2\n")
                     f.write(f"{to_timestamp(duration-subtitle_display_time)} --> {to_timestamp(duration)}\n")
-                    f.write(f"{os.path.splitext(filename)[0]}\n")
-                    f.write("\n")
+                f.write(f"{os.path.splitext(filename)[0]}\n\n")
 
-            print(f"Subtitle file created for {filename}: {subtitle_filename}")
+            filepath = os.path.join(dirpath, filename)
+            srt_filename = os.path.splitext(filepath)[0] + ".srt"
+
+            print(f"\033[36mCREATED: {subtitle_filename}")
+            print("\033[1;90m " + '-' * (width-2) + "\033[0;0m")
+            logging.warning(f"CREATED: {srt_filename}" + "\n" + " " + "-" * 198)
 
     # check if scan subdirectories option is selected
     if not scan_subdirs:
