8292bd5a22662067ec8616dda9d90b298ddda33b
DevXen <DevXen@Gmail.com>
2023-03-06 16:04:56 -0700

    updated a few errors in the srt files and arguments
    Fixed srt files to be valid srt formatted. fixed arguments so when position and default was selected it would ignore the position and only do default. that's been fixed. and fixed it so it didn't add extra timestamps if start or end was selected.

--- Old Versions/MusicVideoTitleSubtitles v1.7 (03.06.2023).py
+++ Old Versions/MusicVideoTitleSubtitles v1.7 (03.06.2023).py
@@ -1,4 +1,4 @@
-# Version 1.8
+# Version 1.7
 
 import os
 import subprocess
@@ -38,45 +38,37 @@ skipped_count = 0
 # function to convert time in seconds to subtitle timestamp format
 def to_timestamp(seconds):
     """Converts seconds to SRT timestamp format."""
-    hours = int(seconds // 3600)
-    minutes = int(seconds // 60 % 60)
-    seconds = int(seconds % 60)
-    milliseconds = int((seconds % 1) * 1000)
-    return f"{hours:02}:{minutes:02}:{seconds:02},{milliseconds:03}"
-
+    return f"{str(int(seconds//60)).zfill(2)}:{str(int(seconds%60)).zfill(2)},{str(int((seconds%1)*1000)).zfill(3)}"
 
 # parse command line arguments
 parser = argparse.ArgumentParser(description='Create subtitles for video files in a directory.')
 parser.add_argument('-d', '--directory', help='directory path (default is current directory)')
 parser.add_argument('-s', '--subdirs', action='store_true', help='scan subdirectories')
-parser.add_argument('-t', '--time', help='subtitle display time in seconds, or \'full\' [Default: 10s]')
-parser.add_argument('-p', '--position', choices=['start', 'end', 'both'], help='subtitle position [Default: Both]')
+parser.add_argument('-t', '--time', help='subtitle display time in seconds, or \'full\' (default is 10)')
+parser.add_argument('-p', '--position', choices=['start', 'end', 'both'], help='subtitle position (default is both)')
 parser.add_argument('-r', '--remove', action='store_true', help='remove existing subtitle files')
 parser.add_argument('-df', '--default', action='store_true', help='skip user prompts and use defaults')
 
 args = parser.parse_args()
 
 # set defaults
-directory = '.' if args.default else args.directory or input("Enter directory path [Default: Current Directory]: ") or '.'
+directory = '.' if args.default else args.directory or input("Enter directory path [default is current directory]: ") or '.'
 scan_subdirs = True if args.subdirs or args.default else input("Scan subdirectories? [Y/n]: ").lower() in ['y', '']
-
+#subtitle_display_time = '10' if args.default else args.time or input("Enter subtitle display time in seconds, or 'full' [default is 10]: ").lower()
 if args.time:
     subtitle_display_time = args.time
 else: 
     subtitle_display_time = '10'
 
-subtitle_display_timews = '10' if args.default else args.time or input("Enter subtitle display time in seconds, or 'full' [Default: 10s]: ").lower()
+subtitle_display_timews = '10' if args.default else args.time or input("Enter subtitle display time in seconds, or 'full' [default is 10]: ").lower()
 
 if subtitle_display_time not in ['f', 'full']:
     try:
         subtitle_display_time = int(subtitle_display_time)
     except ValueError:
         subtitle_display_time = 10
-
-if args.default and args.position:
-    subtitle_position = args.position
-else:
-    subtitle_position = args.position or 'both'
+subtitle_position = 'both' if args.default else args.position or input("Enter subtitle position, 'start', 'end', or 'both' (default is 'both'): ").lower()
+subtitle_position = 'start' if subtitle_position in ['s', 'start'] else 'end' if subtitle_position in ['e', 'end'] else 'both'
 
 # remove existing subtitle files if requested
 if args.remove:
@@ -102,8 +94,8 @@ if args.remove:
                 print(f"\033[1;90m| \033[0;0m\033[91mDELETED\033[1;90m: {filename}\033[1;90m" + " " * (width-(len(filename))-12) + "|\033[0;0m")
                 logger.info(f"DELETED: {srt_filename}" + "\n" + " " + "-" * 198)
     print("\033[1;90m " + '=' * (width-2) + "\033[0;0m")
-
 # loop through files in directory (and subdirectories if selected)
+
 for dirpath, dirnames, filenames in os.walk(directory):
     for filename in filenames:
         # check if file is a video file
@@ -133,29 +125,20 @@ for dirpath, dirnames, filenames in os.walk(directory):
             result = subprocess.run(command, capture_output=True, text=True)
             duration = float(result.stdout.strip())
 			
-
-
             # create subtitle file
             with open(os.path.join(dirpath, subtitle_filename), "w", encoding='utf-8') as f:
-                if subtitle_display_time == 'full' or subtitle_display_time == 'f':
-                    f.write("1\n")
+                f.write("1\n")
+                if subtitle_display_time == 'full':
                     f.write(f"{to_timestamp(0)} --> {to_timestamp(duration)}\n")
-                    f.write(f"{os.path.splitext(filename)[0]}\n\n")
                 elif subtitle_position == 'start':
-                    f.write("1\n")
                     f.write(f"{to_timestamp(0)} --> {to_timestamp(subtitle_display_time)}\n")
-                    f.write(f"{os.path.splitext(filename)[0]}\n\n")
                 elif subtitle_position == 'end':
-                    f.write("1\n")
                     f.write(f"{to_timestamp(duration-subtitle_display_time)} --> {to_timestamp(duration)}\n")
-                    f.write(f"{os.path.splitext(filename)[0]}\n\n")
                 else:
-                    f.write("1\n")
                     f.write(f"{to_timestamp(0)} --> {to_timestamp(subtitle_display_time)}\n")
-                    f.write(f"{os.path.splitext(filename)[0]}\n\n")
                     f.write("2\n")
                     f.write(f"{to_timestamp(duration-subtitle_display_time)} --> {to_timestamp(duration)}\n")
-                    f.write(f"{os.path.splitext(filename)[0]}\n\n")
+                f.write(f"{os.path.splitext(filename)[0]}\n\n")
 
             filepath = os.path.join(dirpath, filename)
             srt_filename = os.path.splitext(filepath)[0] + ".srt"
@@ -172,6 +155,8 @@ for dirpath, dirnames, filenames in os.walk(directory):
             print(f"\033[1;90m| \033[0;0m\033[36mCREATED\033[1;90m: {subtitle_filename}\033[1;90m" + " " * (width-(len(subtitle_filename))-12) + "|\033[0;0m")
 
             logger.info(f"CREATED: {srt_filename}" + "\n" + " " + "-" * 198)
+
+
 
 print("\033[1;90m " + '=' * (width-2) + "\033[0;0m")
 print("\n\n\n\033[1;90m " + '=' * (width-2) + "\033[0;0m")
